6cb1b73486f5e3005202109b28b995813560f300
Ryota Enoki <ryoenoki2013@gmail.com>
2022-08-16 16:28:23 +0900

    web service

--- lib/webservice/webservice.dart
+++ lib/webservice/webservice.dart
@@ -1,7 +1,18 @@
+import 'dart:convert';
+
 import 'package:movie_app_mvvm/movie_apikey.dart';
+import 'package:http/http.dart' as http;
 
 class Webservice {
   Future<List<Movie>> fetchMovies(String keyword) async {
     final url = 'http://www.omdbapi.com/?s=$keyword&apikey=$apikey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      final Iterable json = body["Search"];
+      return json.map((movie) => Movie.fromJson(movie)).toList();
+    } else {
+      throw Exception("Unable to perfomr request!");
+    }
   }
 }
