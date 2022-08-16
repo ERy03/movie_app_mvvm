281156feb331cfaf87dc26fc94bd15c36a98a567
Ryota Enoki <ryoenoki2013@gmail.com>
2022-08-16 18:12:08 +0900

    provider

--- lib/view_model/movie_list_view_model.dart
+++ lib/view_model/movie_list_view_model.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app_mvvm/view_model/movie_view_model.dart';
+import 'package:movie_app_mvvm/webservice/webservice.dart';
 
 class MovieListViewModel extends ChangeNotifier {
   List<MovieViewModel> movies = <MovieViewModel>[];
 
-  Future<void> fetchMovies(String keyword) async {}
+  Future<void> fetchMovies(String keyword) async {
+    final results = await Webservice().fetchMovies(keyword);
+    movies = results.map((item) => MovieViewModel(movie: item)).toList();
+    print(movies);
+    notifyListeners();
+  }
 }
