048470183d4ac063135845d848a0800f9a834fdc
Ryota Enoki <ryoenoki2013@gmail.com>
2022-08-16 18:21:04 +0900

    view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app_mvvm/view/movie_list_page.dart';
 import 'package:provider/provider.dart';
 
 import 'view_model/movie_list_view_model.dart';
